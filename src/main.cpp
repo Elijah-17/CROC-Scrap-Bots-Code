55a6f6a35dc41f86278de88c3f7935feb5e1c102
Elijah <elijahreichart@gmial.com>
2026-01-31 01:15:23 -0500

    attempt to ttry to get unhappy c3 to work

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,18 @@
 #include <Arduino.h>
-
 void setup() {
-  Serial.begin(115200);
-  delay(5000);  // allow USB UART to initialize
-  Serial.println("Serial test successful!");
+  Serial.begin(115200);      // use Serial for USB/UART
+  delay(3000);               // wait for Serial Monitor to attach
+  pinMode(LED_BUILTIN, OUTPUT);
+  Serial.println("ESP32-C3 test board ready!");
 }
 
 void loop() {
-  Serial.println("Tick");
-  delay(1000);
+  digitalWrite(LED_BUILTIN, HIGH);
+  Serial.println("LED ON");    // print status to serial
+  delay(500);
+  
+  digitalWrite(LED_BUILTIN, LOW);
+  Serial.println("LED OFF");   // print status to serial
+  delay(500);
 }
+
